9627e5929bd259e705055f3c974f4d87c070b62b
naganathar <sarvannaganathar@gmail.com>
2025-05-12 21:34:19 +0200

    made the program usable with title input

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
+import sys
 from stats import *
+
+def main():
+    if len(sys.argv) < 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = "books/" + sys.argv[1] + ".txt"
+    try:
+        text = get_book_text(filepath)
+        num_words = get_num_words(text)
+        #num_chars = get_chars_dict(text)
+        char_list = get_sorted_chars_list(text)
+        print_report(filepath, num_words, char_list)
+    except FileNotFoundError:
+        print(f"Error: File {filepath} not found.")
+        sys.exit(1)
+    except Exception as e:
+        print(f"An unexpected error occured: {e}")
+        sys.exit(1)
 
 def get_book_text(filepath):
     with open(filepath) as f:
-        file_contents = f.read()
-        return file_contents
+        return f.read()
 
 def print_report(filepath, num_words, char_list):
     REPORT_WIDTH = 33
@@ -25,12 +44,4 @@ def print_report(filepath, num_words, char_list):
 
     print_header("End", "=")
 
-def main():
-    filepath = "books/frankenstein.txt"
-    text = get_book_text(filepath)
-    num_words = get_num_words(text)
-    num_chars = get_chars_dict(text)
-    char_list = get_sorted_chars_list(text)
-
-    print_report(filepath, num_words, char_list)
 main()
