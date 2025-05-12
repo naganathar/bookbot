ddbd04ddde390b3931c86453cf1131fd5112fd96
naganathar <sarvannaganathar@gmail.com>
2025-05-12 19:53:15 +0200

    created list sorter and report functions

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-from stats import get_num_words, get_chars_dict
+from stats import *
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
 
+def print_report(filepath, num_words, char_list):
+    REPORT_WIDTH = 33
+
+    def print_header(title, separator):
+        print(f"{' ' + title + ' ':{separator}^{REPORT_WIDTH}}")
+
+    print_header("BOOKBOT", "=")
+    print(f"Analyzing book found at {filepath}...")
+
+    print_header("Word Count", "-")
+    print(f"Found {num_words} total words")
+
+    print_header("Character Count", "-")
+    for item in char_list:
+        char = item["char"]
+        num = item["num"]
+        print(f"{char}: {num}")
+
+    print_header("End", "=")
+
 def main():
-    frankenstein = "books/frankenstein.txt"
-    text = get_book_text(frankenstein)
+    filepath = "books/frankenstein.txt"
+    text = get_book_text(filepath)
     num_words = get_num_words(text)
     num_chars = get_chars_dict(text)
+    char_list = get_sorted_chars_list(text)
 
-    #print(f"{num_words} words found in the documents")
-    print(f"{num_chars}")
+    print_report(filepath, num_words, char_list)
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,15 @@ def get_chars_dict(text):
             chars[c] = 0
         chars[c] += 1
     return(chars)
+
+def sort_on(dict):
+    return dict["num"]
+
+def get_sorted_chars_list(text):
+    chars = get_chars_dict(text)
+    chars_list = []
+    for char, num in chars.items():
+        if char.isalpha():
+            chars_list.append({"char":char, "num":num})
+    chars_list.sort(reverse=True, key=sort_on)
+    return chars_list
